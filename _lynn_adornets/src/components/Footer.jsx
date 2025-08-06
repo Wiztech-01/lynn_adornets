72692f9ae0469a146c3e4e96c3bfd7381cccffa0
Property Mkononi <propertymkononi@gmail.com>
2025-08-06 23:03:54 +0300

    add footer

---
+++ _lynn_adornets/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Lynn Adornets. All rights reserved.</p>
+      <div className="socials">
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">Instagram</a>
+        <a href="https://tiktok.com" target="_blank" rel="noreferrer">TikTok</a>
+        <a href="https://wa.me/254700060496" target="_blank" rel="noreferrer">WhatsApp</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
