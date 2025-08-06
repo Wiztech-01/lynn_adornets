cfef762b730364f5f2f2054de78371965a173ce4
Property Mkononi <propertymkononi@gmail.com>
2025-08-06 22:44:38 +0300

    add commit

--- _lynn_adornets/src/main.jsx
+++ _lynn_adornets/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // if you're using Tailwind or global CSS
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
